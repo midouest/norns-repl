4d0ce0689582a22f67201bbcee7fe26049b9a331
Robin <midouesterner@gmail.com>
2021-02-11 22:14:33 -0600

    chore: tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -9,80 +9,80 @@ let ws: WebSocket | undefined;
 let term: vscode.Terminal | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-	info('activate');
-	let disposable = vscode.commands.registerCommand('norns-repl.connect', connectCommand);
-	context.subscriptions.push(disposable);
+    info('activate');
+    let disposable = vscode.commands.registerCommand('norns-repl.connect', connectCommand);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {
-	info('deactivate');
-	cleanup();
+    info('deactivate');
+    cleanup();
 }
 
 function connectCommand(): void {
-	info('connect');
+    info('connect');
 
-	if (term !== undefined) {
-		term.show();
-		return;
-	}
+    if (term !== undefined) {
+        term.show();
+        return;
+    }
 
-	const status = vscode.window.createStatusBarItem();
-	status.text = 'Connecting to Matron...';
-	status.show();
+    const status = vscode.window.createStatusBarItem();
+    status.text = 'Connecting to Matron...';
+    status.show();
 
-	const { host, port } = vscode.workspace.getConfiguration('norns-repl.connect');
-	ws = new WebSocket(`ws://${host}:${port}`, ['bus.sp.nanomsg.org']);
+    const { host, port } = vscode.workspace.getConfiguration('norns-repl.connect');
+    ws = new WebSocket(`ws://${host}:${port}`, ['bus.sp.nanomsg.org']);
 
-	const rx = new Subject<string>();
+    const rx = new Subject<string>();
 
-	ws.on('message', (data) => {
-		rx.next(data.toString());
-	});
+    ws.on('message', (data) => {
+        rx.next(data.toString());
+    });
 
-	ws.on('close', (code, reason) => {
-		info('websocket close', code, reason);
-		cleanup();
-	});
+    ws.on('close', (code, reason) => {
+        info('websocket close', code, reason);
+        cleanup();
+    });
 
-	ws.on('error', (err) => {
-		error('websocket error', err);
-		status.hide();
-		const msg = `Error connecting to Matron: ${err.message}`;
-		vscode.window.showErrorMessage(msg);
-		cleanup();
-	});
+    ws.on('error', (err) => {
+        error('websocket error', err);
+        status.hide();
+        const msg = `Error connecting to Matron: ${err.message}`;
+        vscode.window.showErrorMessage(msg);
+        cleanup();
+    });
 
-	ws.on('open', () => {
-		info('websocket open');
-		status.hide();
+    ws.on('open', () => {
+        info('websocket open');
+        status.hide();
 
-		const repl = new NornsREPL(rx.asObservable());
+        const repl = new NornsREPL(rx.asObservable());
 
-		repl.tx$.subscribe((data) => {
-			ws?.send(data);
-		});
+        repl.tx$.subscribe((data) => {
+            ws?.send(data);
+        });
 
-		repl.close$.subscribe(() => {
-			cleanup();
-		});
+        repl.close$.subscribe(() => {
+            cleanup();
+        });
 
-		term = vscode.window.createTerminal({
-			name: 'norns',
-			pty: repl,
-		});
-		term.show();
-	});
+        term = vscode.window.createTerminal({
+            name: 'norns',
+            pty: repl,
+        });
+        term.show();
+    });
 }
 
 function cleanup(): void {
-	if (term !== undefined) {
-		term.dispose();
-		term = undefined;
-	}
+    if (term !== undefined) {
+        term.dispose();
+        term = undefined;
+    }
 
-	if (ws !== undefined) {
-		ws.close();
-		ws = undefined;
-	}
+    if (ws !== undefined) {
+        ws.close();
+        ws = undefined;
+    }
 }
